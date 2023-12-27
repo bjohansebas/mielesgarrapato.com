b548ad404e2fb24f2a3089b079ab84b43d044112
Sebastian Beltran <bjohansebas@gmail.com>
2023-12-27 12:55:46 -0500

    feat: change style of error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,18 +1,17 @@
 'use client'
 
-export default function Error({ reset }: { reset: () => void }) {
+import { Button } from '@ui/ui/button'
+
+export default function ErrorLayout({ reset }: { reset: () => void }) {
   return (
-    <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12">
-      <h2 className="text-xl font-bold">Oh no!</h2>
+    <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-buttercup-400 bg-white p-8 md:p-12">
+      <h2 className="text-2xl font-bold">Oh no!</h2>
       <p className="my-2">
         There was an issue with our storefront. This could be a temporary issue, please try your action again.
       </p>
-      <button
-        className="mx-auto mt-4 flex w-full items-center justify-center rounded-full bg-blue-600 p-4 tracking-wide text-white hover:opacity-90"
-        onClick={() => reset()}
-      >
+      <Button className="py-4 mt-4" onClick={() => reset()}>
         Try Again
-      </button>
+      </Button>
     </div>
   )
 }
