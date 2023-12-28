f3671ef5db936433ec7a9a2d9a9568de563713dd
Sebastian Beltran <bjohansebas@gmail.com>
2023-12-28 12:39:12 -0500

    feat: improve style in dark of modal cart

--- src/components/cart/modal.tsx
+++ src/components/cart/modal.tsx
@@ -1,16 +1,18 @@
 'use client'
 
-import { Dialog, Transition } from '@headlessui/react'
 import { ShoppingCartIcon } from '@heroicons/react/24/outline'
-import { DEFAULT_OPTION } from '@lib/constants'
-import type { Cart } from '@lib/shopify/types'
-import { createUrl } from '@lib/utils'
 import Price from '@ui/price'
 import { Button } from '@ui/ui/button'
 import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@ui/ui/sheet'
+
 import Image from 'next/image'
 import Link from 'next/link'
-import { Fragment, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
+import { DEFAULT_OPTION } from '@lib/constants'
+import type { Cart } from '@lib/shopify/types'
+import { createUrl } from '@lib/utils'
+
 import { DeleteItemButton } from './delete-item-button'
 import { EditItemQuantityButton } from './edit-item-quantity-button'
 import OpenCart from './open-cart'
@@ -59,14 +61,14 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
             ) : (
               <div className="flex h-full flex-col justify-between overflow-hidden p-1">
                 <ul className="flex-grow overflow-auto py-4">
-                  {cart.lines.map((item, i) => {
+                  {cart.lines.map((item) => {
                     const merchandiseSearchParams = {} as MerchandiseSearchParams
 
-                    item.merchandise.selectedOptions.forEach(({ name, value }) => {
+                    for (const { name, value } of item.merchandise.selectedOptions) {
                       if (value !== DEFAULT_OPTION) {
                         merchandiseSearchParams[name.toLowerCase()] = value
                       }
-                    })
+                    }
 
                     const merchandiseUrl = createUrl(
                       `/product/${item.merchandise.product.handle}`,
@@ -74,7 +76,7 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                     )
 
                     return (
-                      <li key={i} className="flex w-full flex-col border-b border-neutral-300">
+                      <li key={item.id} className="flex w-full flex-col">
                         <div className="flex w-full flex-row px-1 py-4 gap-6">
                           <Link href={merchandiseUrl} onClick={closeCart}>
                             <div className="h-28 w-28 cursor-pointer overflow-hidden rounded-[1rem]">
@@ -89,26 +91,28 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                           </Link>
                           <div className="flex h-16 flex-col justify-between">
                             <Link href={merchandiseUrl} onClick={closeCart}>
-                              <div className="flex flex-1 flex-col text-base">
-                                <span className="leading-tight">{item.merchandise.product.title}</span>
+                              <div className="flex flex-1 flex-col text-base mb-1">
+                                <span className="leading-tight text-xl hover:text-buttercup-500 transition-all duration-200 ease font-semibold">
+                                  {item.merchandise.product.title}
+                                </span>
                                 {item.merchandise.title !== DEFAULT_OPTION ? (
                                   <p className="text-sm text-neutral-400">{item.merchandise.title}</p>
                                 ) : null}
                               </div>
                             </Link>
                             <Price
-                              className="flex justify-end space-y-2 text-right text-sm"
+                              className="flex text-sm mb-4"
                               amount={item.cost.totalAmount.amount}
                               currencyCode={item.cost.totalAmount.currencyCode}
                             />
-                            <div className="ml-auto flex h-9 flex-row items-center rounded-full border border-neutral-200 dark:border-neutral-700">
-                              <EditItemQuantityButton item={item} type="minus" />
-                              <p className="w-6 text-center">
-                                <span className="w-full text-sm">{item.quantity}</span>
-                              </p>
-                              <EditItemQuantityButton item={item} type="plus" />
-                            </div>
-                            <div>
+                            <div className="flex gap-2">
+                              <div className="ml-auto flex h-9 flex-row items-center rounded-full border border-neutral-200 dark:border-neutral-700">
+                                <EditItemQuantityButton item={item} type="minus" />
+                                <p className="w-6 text-center">
+                                  <span className="w-full text-sm">{item.quantity}</span>
+                                </p>
+                                <EditItemQuantityButton item={item} type="plus" />
+                              </div>
                               <DeleteItemButton item={item} />
                             </div>
                           </div>
@@ -117,34 +121,17 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                     )
                   })}
                 </ul>
-                <div className="py-4 text-sm text-neutral-500 dark:text-neutral-400">
-                  <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 dark:border-neutral-700">
-                    <p>Taxes</p>
+                <div className="py-4 text-sm text-neutral-400">
+                  <div className="mb-3 text-xl flex items-center gap-1 justify-center">
+                    <p className="text-muted-foreground">Subtotal:</p>
                     <Price
-                      className="text-right text-base text-black dark:text-white"
-                      amount={cart.cost.totalTaxAmount.amount}
-                      currencyCode={cart.cost.totalTaxAmount.currencyCode}
-                    />
-                  </div>
-                  <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1 dark:border-neutral-700">
-                    <p>Shipping</p>
-                    <p className="text-right">Calculated at checkout</p>
-                  </div>
-                  <div className="mb-3 flex items-center justify-between border-b border-neutral-200 pb-1 pt-1 dark:border-neutral-700">
-                    <p>Total</p>
-                    <Price
-                      className="text-right text-base text-black dark:text-white"
+                      className="text-foreground"
                       amount={cart.cost.totalAmount.amount}
                       currencyCode={cart.cost.totalAmount.currencyCode}
                     />
                   </div>
+                  <p className="text-center">Taxes & shipping calculated at checkout</p>
                 </div>
-                <a
-                  href={cart.checkoutUrl}
-                  className="block w-full rounded-full bg-blue-600 p-3 text-center text-sm font-medium text-white opacity-90 hover:opacity-100"
-                >
-                  Proceed to Checkout
-                </a>
               </div>
             )}
             <SheetFooter>
@@ -153,7 +140,11 @@ export default function CartModal({ cart }: { cart: Cart | undefined }) {
                   <Button type="submit" className="w-full uppercase font-bold">
                     Continue shopping
                   </Button>
-                ) : null}
+                ) : (
+                  <Button asChild className="p-3 w-full text-lg h-[27]">
+                    <a href={cart.checkoutUrl}>Proceed to Checkout</a>
+                  </Button>
+                )}
               </SheetClose>
             </SheetFooter>
           </div>
