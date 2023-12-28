d3497718af5600431575d7a77018c88888afcf71
Sebastian Beltran <bjohansebas@gmail.com>
2023-12-28 11:11:49 -0500

    fix: fix backgroud of card error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,7 +4,7 @@ import { Button } from '@ui/ui/button'
 
 export default function ErrorLayout({ reset }: { reset: () => void }) {
   return (
-    <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-buttercup-400 bg-white p-8 md:p-12">
+    <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-buttercup-400 p-8 md:p-12">
       <h2 className="text-2xl font-bold">Oh no!</h2>
       <p className="my-2">
         There was an issue with our storefront. This could be a temporary issue, please try your action again.
