858216f31125731ad21b2e6e075189eeaaa6b693
Sebastian Beltran <bjohansebas@gmail.com>
2024-01-15 17:20:42 -0500

    feat: remove carousel and three item grid component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Carousel } from '@ui/carousel'
-import { ThreeItemGrid } from '@ui/grid/three-items'
 import Footer from '@ui/layout/footer'
 import { Suspense } from 'react'
 
@@ -8,9 +6,7 @@ export const runtime = 'edge'
 export default async function HomePage() {
   return (
     <>
-      <ThreeItemGrid />
       <Suspense>
-        <Carousel />
         <Suspense>
           <Footer />
         </Suspense>
